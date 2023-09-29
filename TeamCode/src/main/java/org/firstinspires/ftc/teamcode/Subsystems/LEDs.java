9c9ffa56794c4dae5efb55dd34c684d9eeeaceba
dgreathouse <darryl.greathouse@gmail.com>
2023-09-29 06:00:55 -0400

    Added arm stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/LEDs.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+public class LEDs
+{
+    RevColorSensorV3 m_clawLeft;
+    RevColorSensorV3 m_clawRight;
+    RevColorSensorV3 m_chassisStop;
+
+}
